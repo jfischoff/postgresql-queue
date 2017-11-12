0636133de2502f1d22bb471763777fdf6afbcdeb
Jonathan Fischoff <jfischoff@yahoo.com>
2017-11-12 02:06:08 -0500

    hspec and better index

--- src/Database/PostgreSQL/Simple/Queue/Migrate.hs
+++ src/Database/PostgreSQL/Simple/Queue/Migrate.hs
@@ -65,9 +65,7 @@ migrate schemaName conn = void $ execute_ conn $
 
   CREATE INDEX active_state_idx ON payloads (state)
     WHERE (state = 'enqueued');
-  CREATE INDEX active_attempts_idx ON payloads (attempts)
-    WHERE (state = 'enqueued');
-  CREATE INDEX active_created_at_idx ON payloads (created_at)
+  CREATE INDEX active_created_at_attempts_idx ON payloads (created_at, attempts)
     WHERE (state = 'enqueued');
 
   CREATE TRIGGER payloads_modified
